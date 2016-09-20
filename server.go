bcf9ffabe55c0a0543ddd40d1eba78901acde73c
diginatu <diginatu.510o@gmail.com>
2016-09-20 22:18:22 +0900

    keep the message UTF-8 valid

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"unicode/utf8"
 
 	"golang.org/x/net/websocket"
 )
@@ -19,18 +20,81 @@ var (
 	ngmw io.WriteCloser
 )
 
-// BridgeServer the data received on the WebSocket.
+func utf8SafeCopy(dst io.Writer, src io.Reader) (written int64, err error) {
+	var utf8tmp []byte
+	buf := make([]byte, 32*1024)
+
+read:
+	for {
+		// append utf8tmp to the top of buf
+		copy(buf, utf8tmp)
+		// read into the rest buf
+		nr, er := src.Read(buf[len(utf8tmp):])
+		nr += len(utf8tmp)
+		utf8tmp = utf8tmp[0:0]
+		if nr > 0 {
+			rb := buf[0:nr]
+			if !utf8.Valid(rb) {
+				for i := 1; ; i++ {
+					if i > 4 {
+						panic("invalid data (not utf-8)")
+					}
+					if i == nr {
+						utf8tmp = make([]byte, i)
+						copy(utf8tmp, buf[nr-i:nr])
+						break read
+					}
+					rb = buf[0 : nr-i]
+					if utf8.Valid(rb) {
+						utf8tmp = append(utf8tmp, buf[nr-i:nr]...)
+						nr -= i
+						break
+					}
+				}
+			}
+			nw, ew := dst.Write(rb)
+			if nw > 0 {
+				written += int64(nw)
+			}
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er == io.EOF {
+			break
+		}
+		if er != nil {
+			err = er
+			break
+		}
+	}
+	return written, err
+}
+
+// BridgeServer receive a connection
 func BridgeServer(wsc *websocket.Conn) {
+	fmt.Println("connected to a client")
+
 	go func() {
-		_, err := io.Copy(wsc, ngmr)
+		_, err := io.Copy(ngmw, wsc)
 		if err != nil {
-			log.Println(err)
+			fmt.Println(err)
+			return
 		}
 	}()
-	_, err := io.Copy(ngmw, wsc)
+
+	_, err := utf8SafeCopy(wsc, ngmr)
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
+		return
 	}
+
+	fmt.Println("disconnected")
 }
 
 // This example demonstrates a trivial echo server.
